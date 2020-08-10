3b205e95ed73103c65a597ee3dbac44c2f327934
zou_xq <zou_xq@haici.com>
2020-08-10 12:44:57 +0800

    zxq-提交3

--- src/main/java/com/xq/oauth/OAuth.java
+++ src/main/java/com/xq/oauth/OAuth.java
@@ -19,7 +19,7 @@ public class OAuth {
     private String clientSecret; //申请QQ登录成功后，分配给网站的appkey。
     private String redirectUri;  //成功授权后的回调地址，必须是注册appid时填写的主域名下的地址，建议设置为网站首页或网站的用户中心。注意需要将url进行URLEncode。
     private String response_type = "code"; // 	授权类型，此值固定为“code”。
-
+    private String re_type = "q";
     public OAuth() {
     }
 
